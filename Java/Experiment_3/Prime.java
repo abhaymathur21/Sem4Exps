149a480c6963d517259ea8522aeee7331a47b71d
abhaymathur21 <a21.mathur21@gmail.com>
2023-05-16 15:59:09 +0530

    prime using command line arguments

---
+++ Java/Experiment_3/Prime.java
@@ -0,0 +1,20 @@
+
+public class Prime 
+{
+    public static void main(String args[]) //taking input from command line arguments -> javac Prime.java -> java Prime 12 -> Output= not prime
+    {
+        int n = Integer.parseInt(args[0]);
+        System.out.println(n);
+        boolean flag=false;
+        for(int i=2;i<n;i++)
+        {
+            if(n%i==0)
+            {
+                flag=true;
+                break;
+            }
+        }
+        if(flag==false) System.out.println("It is a prime number");
+        else System.out.println("It is not a prime number");
+    }
+}
