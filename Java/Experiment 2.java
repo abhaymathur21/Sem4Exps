571e5b03aa498e6a721c5d17639ddf9f68e45438
abhaymathur21 <a21.mathur21@gmail.com>
2023-05-16 09:45:58 +0530

    geo seq

--- Java/Experiment 2.java
+++
@@ -1,7 +0,0 @@
-class experiment2
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Hi");
-    }
-}
---
+++ Java/Experiment 2/GeometricSequence.java
@@ -0,0 +1,15 @@
+import java.lang.Math;
+class GeometricSequence
+{
+    public static void main(String[] args)
+    {
+        int n=5, r=2, a=1;
+        double result;
+        System.out.print(a);
+        for(int i=2;i<=n;i++)
+        {
+            result = a*Math.pow(r,i-1);
+            System.out.print(", "+(int)result);
+        }
+    }
+}
